97220a96ae746931f486be133194f7883a5d58d0
unknown <sofija.lazarova888@gmail.com>
2025-02-04 12:05:55 +0100

    spring security added

--- src/main/java/mk/ukim/finki/finance/auth/AuthenticationController.java
+++ src/main/java/mk/ukim/finki/finance/auth/AuthenticationController.java
@@ -1,6 +1,7 @@
 package mk.ukim.finki.finance.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,10 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
-    private AuthenticationService authenticationService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest){
-        return ResponseEntity.ok(this.authenticationService.register(request));
+        return ResponseEntity.ok(this.authenticationService.register(registerRequest));
+    }
+
+    @PostMapping("authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest){
+        AuthenticationResponse authenticationResponse = this.authenticationService.authenticate(authenticationRequest);
+        if(authenticationResponse != null){
+            return ResponseEntity.ok(authenticationResponse);
+        }else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 }
--- src/main/java/mk/ukim/finki/finance/auth/AuthenticationRequest.java
+++ src/main/java/mk/ukim/finki/finance/auth/AuthenticationRequest.java
@@ -1,12 +1,14 @@
 package mk.ukim.finki.finance.auth;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class AuthenticationRequest {
 
     private String email;
